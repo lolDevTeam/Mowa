3238e2bcb4c7c827a72d319a4ddbef27d0a6a3fb
Jan Jalinski <jan.jalinski@binaris-informatik.de>
2017-09-07 19:03:45 +0200

    Projektdateien hinzufügen.

---
+++ Mowa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
