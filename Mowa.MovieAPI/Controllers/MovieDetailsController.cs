3962f377c3df1c0b2f3d3ecb8bde4bc588d69370
Jan Jalinski <jan.jalinski@binaris-informatik.de>
2017-09-10 13:40:48 +0200

    added function to get detailed movie information

--- Mowa.MovieAPI/Controllers/MovieDetailsController.cs
+++ Mowa.MovieAPI/Controllers/MovieDetailsController.cs
@@ -10,6 +10,10 @@
 
 namespace Mowa.MovieAPI.Controllers
 {
+    using System;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Mvc;
@@ -17,6 +21,8 @@ namespace Mowa.MovieAPI.Controllers
     using Microsoft.Extensions.Logging;
 
     using Mowa.MovieAPI.Model.Movie;
+
+    using Newtonsoft.Json;
 
     /// <inheritdoc />
     /// <summary>
@@ -58,6 +64,8 @@ namespace Mowa.MovieAPI.Controllers
         {
             this.configuration = configuration;
             this.logger = logger;
+
+            this.movieDbApiKey = this.configuration.GetSection("MovieDB").GetChildren().FirstOrDefault()?.Value;
         }
 
         /// <summary>
@@ -72,7 +80,28 @@ namespace Mowa.MovieAPI.Controllers
         [HttpGet("{id}")]
         public async Task<MovieDetail> Get(int id)
         {
-            var queryUrl = string.Concat(MovieDbApiUrl + "movie/{id}?api_key={api_key}", id, this.movieDbApiKey);
+            var queryUrl = MovieDbApiUrl + id + "?api_key=" + this.movieDbApiKey;
+            this.logger.LogDebug("queryUrl=" + queryUrl);
+
+            var client = new HttpClient() { BaseAddress = new Uri(MovieDbApiUrl) };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            this.logger.LogInformation("Performing a GET request to the endpoint.");
+
+            var response = await client.GetAsync(queryUrl);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                this.logger.LogError("response status is not successful. " + response.StatusCode);
+                return null;
+            }
+
+            var responseData = await response.Content.ReadAsStringAsync();
+
+            var searchResult = JsonConvert.DeserializeObject<MovieDetail>(responseData);
+
+            return searchResult;
         }
     }
 }
