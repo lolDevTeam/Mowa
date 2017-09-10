ae4398f229a71428e918a31244f2c7a3dd01748f
Jan Jalinski <jan.jalinski@binaris-informatik.de>
2017-09-10 13:14:02 +0200

    added the MovieDetailsController

---
+++ Mowa.MovieAPI/Controllers/MovieDetailsController.cs
@@ -0,0 +1,78 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MovieDetailsController.cs" company="jayDev">
+//   Jan Jalinski
+// </copyright>
+// <summary>
+//   Defines the MovieDetailsController type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Mowa.MovieAPI.Controllers
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
+
+    using Mowa.MovieAPI.Model.Movie;
+
+    /// <inheritdoc />
+    /// <summary>
+    /// The movie details controller.
+    /// </summary>
+    [Route("api/[controller]")]
+    public class MovieDetailsController : Controller
+    {
+        /// <summary>
+        /// The movie db api url.
+        /// </summary>
+        private const string MovieDbApiUrl = "https://api.themoviedb.org/3/movie/";
+
+        /// <summary>
+        /// The movie db api key.
+        /// </summary>
+        private readonly string movieDbApiKey;
+
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        private readonly ILogger logger;
+
+        /// <summary>
+        /// The configuration.
+        /// </summary>
+        private IConfiguration configuration;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MovieDetailsController"/> class.
+        /// </summary>
+        /// <param name="configuration">
+        /// The configuration.
+        /// </param>
+        /// <param name="logger">
+        /// The logger.
+        /// </param>
+        public MovieDetailsController(IConfiguration configuration, ILogger<MovieDetailsController> logger)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// The get.
+        /// </summary>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        [HttpGet("{id}")]
+        public async Task<MovieDetail> Get(int id)
+        {
+            var queryUrl = string.Concat(MovieDbApiUrl + "movie/{id}?api_key={api_key}", id, this.movieDbApiKey);
+        }
+    }
+}
--- Mowa.MovieAPI/Controllers/MovieSearchController.cs
+++ Mowa.MovieAPI/Controllers/MovieSearchController.cs
@@ -16,8 +16,11 @@ namespace Mowa.MovieAPI.Controllers
     using System.Threading.Tasks;
 
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
     using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.Logging;
 
+    using Mowa.MovieAPI.Model.Movie;
     using Mowa.MovieAPI.Model.Search;
 
     using Newtonsoft.Json;
@@ -32,17 +35,22 @@ namespace Mowa.MovieAPI.Controllers
         /// <summary>
         /// The movie db api url.
         /// </summary>
-        private const string MovieDbApiUrl = "https://api.themoviedb.org/3/";
-
-        /// <summary>
-        /// The configuration.
-        /// </summary>
-        private IConfiguration configuration;
+        private const string MovieDbApiUrl = "https://api.themoviedb.org/3/search/movie";
 
         /// <summary>
         /// The movie db api key.
         /// </summary>
-        private string movieDbApiKey;
+        private readonly string movieDbApiKey;
+
+        /// <summary>
+        /// The configuration.
+        /// </summary>
+        private readonly IConfiguration configuration;
+
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        private readonly ILogger logger;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="MovieSearchController"/> class.
@@ -50,9 +58,13 @@ namespace Mowa.MovieAPI.Controllers
         /// <param name="configuration">
         /// The configuration.
         /// </param>
-        public MovieSearchController(IConfiguration configuration)
+        /// <param name="logger">
+        /// The logger.
+        /// </param>
+        public MovieSearchController(IConfiguration configuration, ILogger<MovieSearchController> logger)
         {
             this.configuration = configuration;
+            this.logger = logger;
 
             this.movieDbApiKey = this.configuration.GetSection("MovieDB").GetChildren().FirstOrDefault()?.Value;
         }
@@ -70,16 +82,20 @@ namespace Mowa.MovieAPI.Controllers
         public async Task<SearchResult> Get(string searchString)
         {
             searchString = searchString.Replace(" ", "%20");
-            var queryUrl = MovieDbApiUrl + "search/movie?query=" + searchString + "&api_key=" + this.movieDbApiKey;
+            var queryUrl = MovieDbApiUrl + "?query=" + searchString + "&api_key=" + this.movieDbApiKey;
+
+            this.logger.LogDebug("queryUrl=" + queryUrl);
 
             var client = new HttpClient() { BaseAddress = new Uri(MovieDbApiUrl) };
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
+            this.logger.LogInformation("Performing a GET request to the endpoint.");
             var response = await client.GetAsync(queryUrl);
 
             if (!response.IsSuccessStatusCode)
             {
+                this.logger.LogError("response status is not successful. " + response.StatusCode);
                 return null;
             }
 
